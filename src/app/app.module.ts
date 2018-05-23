7aba22f17def9afc391a63f4b6e22619840db666
Simon Gysin <simon.gysin@students.fhnw.ch>
2018-05-23 09:18:43 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { PostenPage } from '../pages/posten/posten';
+import { RanglistePage } from '../pages/rangliste/rangliste';
+import { TeilnehmerPage } from '../pages/teilnehmer/teilnehmer';
+import { PostendetailPage } from '../pages/postendetail/postendetail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
+import {HttpModule} from '@angular/http'; 
+import {AngularFireDatabaseModule} from 'angularfire2/database'; 
+import {AngularFireModule} from 'angularfire2';
+import { PostenServiceProvider } from '../providers/posten-service/posten-service';
+import { ResultatServiceProvider } from '../providers/resultat-service/resultat-service';
+import { TeilnehmerServiceProvider } from '../providers/teilnehmer-service/teilnehmer-service'; 
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAHYqg1dms1XFObxThuA3_rQWS4X8YybTs",
+  authDomain: "postenlauf-e372d.firebaseapp.com",
+  databaseURL: "https://postenlauf-e372d.firebaseio.com",
+  projectId: "postenlauf-e372d",
+  storageBucket: "postenlauf-e372d.appspot.com",
+  messagingSenderId: "431303393950"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PostenPage,
+    RanglistePage,
+    TeilnehmerPage,
+    PostendetailPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule, 
+    AngularFireDatabaseModule, 
+    AngularFireModule.initializeApp(config),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PostenPage,
+    RanglistePage,
+    TeilnehmerPage,
+    PostendetailPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseServiceProvider,
+    PostenServiceProvider,
+    ResultatServiceProvider,
+    TeilnehmerServiceProvider
   ]
 })
 export class AppModule {}
